44ff18d6878ed8cc9f61567468ba66931225797e
immutable-integer-8885 <gidelbusto@gmail.com>
2020-05-03 03:45:38 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,14 +1,14 @@
 # code your #position_taken? method here!
 
 def position_taken?(board,index)
-  if board[index] == " "
-    false
+  if board[index] == "X" or board[index] == "O"
+    true
   elsif board[index] == ""
     false
   elsif board[index] == nil
     false
-  else board[index] == "X" or board[index] == "O"
-    true
+  else  board[index] == " "
+    false
   end 
 end
     
